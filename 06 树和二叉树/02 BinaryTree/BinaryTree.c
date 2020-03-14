6c4156900cc5234d6a576424dce574b295688afd
Zeng Yong <zengyonghust@foxmail.com>
2020-03-15 01:21:48 +0800

    [主题]节点遍历 [内容]节点各种遍历方式实现 [编号]RTFSC

--- 06 树和二叉树/02 BinaryTree/BinaryTree.c
+++ 06 树和二叉树/02 BinaryTree/BinaryTree.c
@@ -67,157 +67,277 @@ int BiTreeDepth(BiTree T)
 
 TElemType Parent(BiTree T, TElemType e)
 {
-    BiTree Queue[100] = {0};
+    BiTNode Queue[100] = {0};
     int i = 0;
     if (T == NULL || T->data == e) {
         return 0;
     }
-    Queue[i] = T;
+    Queue[i] = *T;
     while(i >= 0) {
-        while (Queue[i]->lchild) {
-            Queue[i + 1] = Queue[i]->lchild;
-            if (Queue[i + 1]->data == e) {
-                return Queue[i]->data;
+        while (Queue[i].lchild) {
+            if (Queue[i].lchild->data == e) {
+                return Queue[i].data;
             }
-            Queue[i]->lchild = NULL;
+            Queue[i + 1] = *Queue[i].lchild;
+            Queue[i].lchild = NULL;
             ++i;
         }
-        if (Queue[i]->rchild) {
-            Queue[i + 1] = Queue[i]->rchild;
-            if (Queue[i + 1]->data == e) {
-                return Queue[i]->data;
+        if (Queue[i].rchild) {
+            if (Queue[i].rchild->data == e) {
+                return Queue[i].data;
             }
-            Queue[i]->rchild = NULL;
+            Queue[i] = *Queue[i].rchild;  // i节点位置无效，不再需要
             ++i;
         }
-        while (Queue[i]->lchild == NULL && Queue[i]->rchild == NULL) {
+        while (Queue[i].lchild == NULL && Queue[i].rchild == NULL) {
             --i;
         }
     }
     return 0;
 }
 
+// 先序前继遍历：父节点->左孩子->右孩子
 TElemType LeftChild(BiTree T, TElemType e)
 {
-    BiTree Queue[100] = {0};
+    BiTNode Queue[100] = {0};
     int i = 0;
     if (T == NULL) {
         return ERROR;
     }
-    Queue[i] = T;
+    Queue[i] = *T;
+    printf("%c", Queue[i].data);
     while (i >= 0) {
-        while (Queue[i]->lchild) {
-            if (Queue[i]->data == e) {
-                return Queue[i]->lchild->data;
+        while (Queue[i].lchild) {
+            if (Queue[i].data == e) {
+                return Queue[i].lchild->data;
             }
-            Queue[i + 1] = Queue[i]->lchild;
-            Queue[i]->lchild = NULL;
+            Queue[i + 1] = *Queue[i].lchild;
+            Queue[i].lchild = NULL;
             ++i;
+            printf("%c", Queue[i].data);
         }
-        if (Queue[i]->data == e) {
+        if (Queue[i].data == e) {
             return 0;
         }
-        if (Queue[i]->rchild) {
-            Queue[i + 1] = Queue[i]->rchild;
-            Queue[i]->rchild = NULL;
-            ++i;
-            if (Queue[i]->data == e) {
-                if (Queue[i]->lchild) {
-                    return Queue[i]->lchild->data;
-                } else {
-                    return 0;
-                }
-            }
+        if (Queue[i].rchild) {
+            Queue[i] = *Queue[i].rchild;
+            printf("%c", Queue[i].data);
+        } else {
+            --i;
         }
-        while ((Queue[i]->lchild == NULL) && (Queue[i]->rchild == NULL)) {
+        while ((Queue[i].lchild == NULL) && (Queue[i].rchild == NULL) && (Queue[i].data != e)) {
             --i;
         }
     }
     return 0;
 }
 
+// 中序前继遍历：左孩子->父节点->右孩子
+Status Traverse_InOrderLR(BiTree T)
+{
+    BiTNode Queue[100] = {0};
+    int i = 0;
+    if (T == NULL) {
+        return ERROR;
+    }
+    Queue[i] = *T;
+    while (i >= 0) {
+        while (Queue[i].lchild) {
+            Queue[i + 1] = *Queue[i].lchild;
+            Queue[i].lchild = NULL;
+            ++i;
+        }
+        printf("%c", Queue[i].data);
+        if (Queue[i].rchild) {
+            Queue[i] = *Queue[i].rchild;
+        } else {
+            --i;
+        }
+        while ((Queue[i].lchild == NULL) && (Queue[i].rchild == NULL)) {
+            printf("%c", Queue[i].data);
+            --i;
+        }
+    }
+    return OK;
+}
+
+// 后序前继遍历：左孩子->右孩子->父节点
+Status Traverse_PostOrderLR(BiTree T)
+{
+    BiTNode Queue[100] = {0};
+    int i = 0;
+    if (T == NULL) {
+        return ERROR;
+    }
+    Queue[i] = *T;
+    while (i >= 0) {
+        while (Queue[i].lchild) {
+            Queue[i + 1] = *Queue[i].lchild;
+            Queue[i].lchild = NULL;
+            ++i;
+        }
+        if (Queue[i].rchild) {
+            Queue[i + 1] = *Queue[i].rchild;
+            Queue[i].rchild = NULL;
+            ++i;
+        }
+        while ((Queue[i].lchild == NULL) && (Queue[i].rchild == NULL)) {
+            printf("%c", Queue[i].data);
+            --i;
+        }
+    }
+    return 0;
+}
+
+// 先序后继遍历：父节点->右孩子->左孩子
 TElemType RightChild(BiTree T, TElemType e)
 {
-    BiTree Queue[100] = {0};
+    BiTNode Queue[100] = {0};
     int i = 0;
     if (T == NULL) {
         return 0;
     }
-    Queue[i] = T;
+    Queue[i] = *T;
     while (i >= 0) {
-        while (Queue[i]->rchild) {
-            if (Queue[i]->data == e) {
-                return Queue[i]->rchild->data;
+        while (Queue[i].rchild) {
+            if (Queue[i].data == e) {
+                return Queue[i].rchild->data;
             }
-            Queue[i + 1] = Queue[i]->rchild;
-            Queue[i]->rchild = NULL;
+            Queue[i + 1] = *Queue[i].rchild;
+            Queue[i].rchild = NULL;
             ++i;
         }
-        if (Queue[i]->data == e) {
+        if (Queue[i].data == e) {
             return 0;
         }
-        if (Queue[i]->lchild) {
-            Queue[i + 1] = Queue[i]->lchild;
-            Queue[i]->lchild = NULL;
-            ++i;
-            if (Queue[i]->data == e) {
-                if (Queue[i]->rchild) {
-                    return Queue[i]->rchild->data;
-                } else {
-                    return 0;
-                }
-            }
+        if (Queue[i].lchild) {
+            Queue[i] = *Queue[i].lchild;
+        } else {
+            --i;
         }
-        while ((Queue[i]->lchild == NULL) && (Queue[i]->rchild == NULL)) {
+        while ((Queue[i].lchild == NULL) && (Queue[i].rchild == NULL) && (Queue[i].data != e)) {
             --i;
         }
     }
     return 0;
 }
 
-TElemType RightChild_1(BiTree T, TElemType e)
+TElemType LeftSibling(BiTree T, TElemType e)
 {
-    BiTree Queue[100] = {0};
+    BiTNode Queue[100] = {0};
     int i = 0;
-    if (T == NULL) {
+    if ((T == NULL) || (T->data == e)) {
         return 0;
     }
-    Queue[i] = T;
+    Queue[i] = *T;
     while (i >= 0) {
-        while (Queue[i]->lchild) {
-            if (Queue[i]->data == e) {
-                if (Queue[i]->rchild) {
-                    return Queue[i]->rchild->data;
-                } else {
-                    return 0;
-                }
+        while (Queue[i].lchild) {
+            if ((Queue[i].rchild) && (Queue[i].rchild->data == e)) {
+                return Queue[i].lchild->data;
             }
-            Queue[i + 1] = Queue[i]->lchild;
-            Queue[i]->lchild = NULL;
-            ++i;
-        }
-        if (Queue[i]->data == e) {
-            if (Queue[i]->rchild) {
-                return Queue[i]->rchild->data;
-            } else {
+            if (Queue[i].lchild->data == e) {
                 return 0;
             }
-        }
-        if (Queue[i]->rchild) {
-            Queue[i + 1] = Queue[i]->rchild;
-            Queue[i]->rchild = NULL;
+            Queue[i + 1] = *Queue[i].lchild;
+            Queue[i].lchild = NULL;
             ++i;
-            if (Queue[i]->data == e) {
-                if (Queue[i]->rchild) {
-                    return Queue[i]->rchild->data;
+        }
+        if (Queue[i].rchild) {
+            Queue[i] = *Queue[i].rchild;
+            if (Queue[i].data == e) {
+                return 0;
+            }
+        } else {
+            --i;
+        }
+        while ((Queue[i].lchild == NULL) && (Queue[i].rchild == NULL)) {
+            --i;
+        }
+    }
+    return 0;
+}
+
+TElemType RightSibling(BiTree T, TElemType e)
+{
+    BiTNode Queue[100] = {0};
+    int i = 0;
+    if ((T == NULL) || (T->data == e)) {
+        return 0;
+    }
+    Queue[i] = *T;
+    while (i >= 0) {
+        while (Queue[i].lchild) {
+            if (Queue[i].lchild->data == e) {
+                if (Queue[i].rchild) {
+                    return Queue[i].rchild->data;
                 } else {
                     return 0;
                 }
             }
+            Queue[i + 1] = *Queue[i].lchild;
+            Queue[i].lchild = NULL;
+            ++i;
         }
-        while ((Queue[i]->lchild == NULL) && (Queue[i]->rchild == NULL)) {
+        if (Queue[i].rchild) {
+            Queue[i] = *Queue[i].rchild;
+            if (Queue[i].data == e) {
+                return 0;
+            }
+        } else {
             --i;
         }
+        while ((Queue[i].lchild == NULL) && (Queue[i].rchild == NULL)) {
+            --i;
+        }
+    }
+    return 0;
+}
+
+BiTree LocationBiTree_1(BiTree T, TElemType e)
+{
+    BiTNode Queue[100] = {0};
+    int i = 0;
+    if ((T == NULL) || (T->data == e)) {
+        return T;
+    }
+    Queue[i] = *T;
+    while (i >= 0) {
+        while (Queue[i].lchild) {
+            if (Queue[i].lchild->data == e) {
+                return Queue[i].lchild;
+            }
+            Queue[i + 1] = *Queue[i].lchild;
+            Queue[i].lchild = NULL;
+            ++i;
+        }
+        if (Queue[i].rchild) {
+            if (Queue[i].rchild->data == e) {
+                return Queue[i].rchild;
+            }
+            Queue[i] = *Queue[i].rchild;
+        } else {
+            --i;
+        }
+        while ((Queue[i].lchild == NULL) && (Queue[i].rchild == NULL)) {
+            --i;
+        }
+    }
+    return NULL;
+}
+
+BiTree LocationBiTree_2(BiTree T, TElemType e)
+{
+    BiTree p = NULL;
+    if ((T == NULL) || (T->data == e)) {
+        return T;
+    } else {
+        if ((p = LocationBiTree_2(T->lchild, e))) {
+            return p;
+        }
+        if ((p = LocationBiTree_2(T->rchild, e))) {
+            return p;
+        }
+        return p;
     }
 }
 
---
+++ 06 树和二叉树/02 BinaryTree/BinaryTreeTest.c
@@ -0,0 +1,14 @@
+#include "BinaryTree.c"
+
+int main(int argc, char *argv[])
+{
+    BiTree T = NULL;
+    FILE *fp = fopen("TestData.txt", "r");
+    CreateBiTree(fp, &T);
+    // char res = LeftChild(T, 'J');
+    // char res = Traverse_InOrderLR(T);
+    char res = Traverse_PostOrderLR(T);
+    putchar('\n');
+    printf("%c\n", res);
+    return 0;
+}
