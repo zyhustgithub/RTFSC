fadc51bd1858f0a09f3f53ae294113f644221dbc
Zeng Yong <zengyonghust@foxmail.com>
2020-03-21 00:15:06 +0800

    [主题]三叉二叉树算法实现完成 [内容]带父节点二叉树相关实现完成 [编号]RTFSC

--- 06 树和二叉树/03 Tri_BinatyTree/Tri_BinaryTree.c
+++ 06 树和二叉树/03 Tri_BinatyTree/Tri_BinaryTree.c
@@ -11,6 +11,9 @@ void Create_Tri(TBiTree *T, FILE *fp)
         *T = NULL;
     } else {
         *T = (TBiTree)malloc(sizeof(TBiTNode));
+        if (!*T) {
+            exit(OVERFLOW);
+        }
         (*T)->data = tmpChar;
         Create_Tri(&((*T)->lchild), fp);
         Create_Tri(&((*T)->rchild), fp);
@@ -112,7 +115,59 @@ TBiTree Position_Tri(TBiTree T, TElemType_Tri e)
 
 void PrintTree_Tri(TBiTree T)
 {
-    // Continue...
+    LinkQueue Q;
+    QElemType_L e;
+    struct {
+        TElemType_Tri data;
+        int len;
+    } num[100] = {0};
+
+    if (T == NULL) {
+        return;
+    }
+    InitQueue_L(&Q);
+    EnQueue_L(&Q, T);
+    int i = 0;
+    int j = 0;
+    num[j].data = T->data;
+    num[j++].len = 1;
+    while (!QueueEmpty_L(Q)) {
+        DeQueue_L(&Q, &e);
+        if (e->lchild) {
+            num[j].data = e->lchild->data;
+            num[j++].len = 2 * num[i].len;
+            EnQueue_L(&Q, e->lchild);
+        }
+        if (e->rchild) {
+            num[j].data = e->rchild->data;
+            num[j++].len = 2 * num[i].len + 1;
+            EnQueue_L(&Q, e->rchild);
+        }
+        ++i;
+    }
+
+    int depth = log(num[j - 1].len) / log(2) + 1;
+    int treeLen = (int)pow(2, depth - 1);
+    int maxLen = (int)pow(2, depth) - 1;
+
+    int row, col, idx;
+    int showArr[100][100] = {0};
+    for (int n = 0; n < j; ++n) {
+        row = log(num[n].len) / log(2) + 1;
+        idx = num[n].len - (int)pow(2, row - 1) + 1;
+        col = treeLen / (int)pow(2, row - 1) + (idx - 1) * 2 * (treeLen / pow(2, row - 1));
+        showArr[row - 1][col - 1] = num[n].data;
+    }
+    for (row = 0; row < depth; ++row) {
+        for (col = 0; col < maxLen; ++col) {
+            if (showArr[row][col] != '\0') {
+                printf("%c", showArr[row][col]);
+            } else {
+                printf(" ");
+            }
+        }
+        putchar('\n');
+    }
 }
 
 #endif
